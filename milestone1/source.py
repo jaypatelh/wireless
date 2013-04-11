6bc103721e8e74397c53ea87484b9f22a5dd3e57
Jay Patel <jayhp9@stanford.edu>
2013-04-11 00:25:00 -0700

    wrote first code for source.py text

--- milestone1/source.py
+++ milestone1/source.py
@@ -5,9 +5,11 @@ from graphs import *
 import binascii
 import random
 
-
-
 class Source:
+    TEXT = 0
+    IMAGE = 1
+    MONOTONE = 2
+
     def __init__(self, monotone, filename=None):
         # The initialization procedure of source object
         self.monotone = monotone
@@ -17,24 +19,57 @@ class Source:
     def process(self):
             # Form the databits, from the filename 
             if self.fname is not None:
-                if self.fname.endswith('.png') or self.fname.endswith('.PNG'):
-                    # Its an image
-                else:           
-                    # Assume it's text                    
-            else:               
+                if self.fname.endswith('.png') or self.fname.endswith('.PNG'): # image
+                    # yo
+                else: # text
+                    size = int(os.path.getsize("testfiles/" + self.fname))
+                    header = self.get_header(size, TEXT)
+                    payload = self.text2bits(self.fname)
+                    databits = header + payload
+            else:
                 # Send monotone (the payload is all 1s for 
-                # monotone bits)   
+                # monotone bits)
             return payload, databits
 
     def text2bits(self, filename):
         # Given a text file, convert to bits
+        f = open("testfiles/" + filename)
+        ascii_array = []
+        # convert each char to its ascii representation first
+        for line in f:
+            for char in line:
+                ascii_array.append(ord(char))
+
+        # convert each ascii number to a 8 bit array
+        bits = []
+        for elem in ascii_array:
+            bits += self.int_to_bit_array(elem, 8)
+
+        return bits
+
+    def int_to_bit_array(self, num, num_bits):
+        bits = []
+        for i in range(num_bits):
+            bits.insert(0, (size >> i) & 1)
         return bits
 
     def bits_from_image(self, filename):
         # Given an image, convert to bits
         return bits
 
-    def get_header(self, payload_length, srctype): 
+    def get_header(self, payload_length, srctype):
         # Given the payload length and the type of source 
         # (image, text, monotone), form the header
+
+        # initialize databits with 0, 0 to signify text file
+        header = []
+        if srctype == TEXT:
+            header = [0, 0]
+        elif srctype == IMAGE:
+            header = [0, 1]
+        elif srctype == MONOTONE:
+            header = [1, 0]
+        
+        # append payload size encoded in next 6 bits
+        header += self.int_to_bit_array(payload_length, 6)
         return header

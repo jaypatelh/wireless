78a288148bb18f94c2e62e6b49561fd2c256ef9e
Angad Singh <angadonline@gmail.com>
2013-05-03 23:38:57 -0700

    Wrote first code for transmitter.py

--- milestone2/transmitter.py
+++ milestone2/transmitter.py
@@ -10,6 +10,7 @@ class Transmitter:
         self.spb = spb
         self.silence = silence
         print 'Transmitter: '
+
     def add_preamble(self, databits):
         '''
         Prepend the array of source bits with silence bits and preamble bits
@@ -19,8 +20,18 @@ class Transmitter:
             [silence bits], [preamble bits], and [databits]
         '''
         # fill in your implementation
+
+        silence_bits = [];
+        for s in range(0, self.silence):
+            silence_bits = silence_bits + [0]
+
+        preamble_bits = self.get_preamble
+        databits_with_preamble = silence_bits + preamble_bits + databits
+
         return databits_with_preamble
 
+    def get_preamble
+        return [1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1]
 
     def bits_to_samples(self, databits_with_preamble):
         '''
@@ -29,6 +40,18 @@ class Transmitter:
         Output should be an array of samples.
         '''
         # fill in your implemenation
+
+        samples_array = []
+
+        for bit_number in range(0, databits_with_preamble.length):
+                if(databits_with_preamble[bit_number] == 0):
+                    for sample_number in range(0, self.spb):
+                        samples_array = samples_array + [0]
+                else if (databits_with_preamble[bit_number] == 1):
+                    for sample_number in range(0, self.spb):
+                        samples_array = samples_array + [one]
+
+        samples = np.array(samples_array)
         return samples
         
 
